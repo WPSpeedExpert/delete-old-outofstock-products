b8eb2cd21c0e20648ac0fb2976317a9cdead844f
WP Speed Expert <139096428+WPSpeedExpert@users.noreply.github.com>
2025-03-17 21:35:31 +0100

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -5,7 +5,7 @@
  * Removes plugin data when uninstalled.
  * 
  * @package Delete Old Out-of-Stock Products
- * @since 1.0.0
+ * @since 2.1.0
  */
 
 // Exit if accessed directly.
@@ -19,10 +19,13 @@ if ($timestamp) {
     wp_unschedule_event($timestamp, 'doop_cron_delete_old_products');
 }
 
+// Remove any background deletion actions
+$timestamp = wp_next_scheduled('oh_doop_background_deletion');
+if ($timestamp) {
+    wp_unschedule_event($timestamp, 'oh_doop_background_deletion');
+}
+
 // Remove the plugin options
 delete_option('oh_doop_options');
-
-// Clean up any transients
-delete_transient('oh_doop_products_to_delete');
-delete_transient('oh_doop_products_total');
-delete_transient('oh_doop_products_deleted');
+delete_option('oh_doop_deletion_running');
+delete_option('oh_doop_last_run_count');
